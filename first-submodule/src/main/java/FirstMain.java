306336b40f146cc41a8b1d750d3d1d095977d7b9
a.borgatin <avborg90@yandex.ru>
2017-03-10 16:04:21 +0300

    добавлен супер сожный код

---
+++ first-submodule/src/main/java/FirstMain.java
@@ -0,0 +1,14 @@
+/**
+ * Created by avborg on 10.03.2017.
+ */
+public class FirstMain {
+
+    public static String getHello(){
+        return "Hello, world!!!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getHello());
+    }
+
+}
---
+++ second-submodule/src/main/java/SecondMain.java
@@ -0,0 +1,9 @@
+/**
+ * Created by avborg on 10.03.2017.
+ */
+public class SecondMain {
+
+    public static void main(String[] args) {
+        System.out.println(FirstMain.getHello());
+    }
+}
